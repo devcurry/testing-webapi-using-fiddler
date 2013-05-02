92e9d122dd5f01e2f09cb4a5bb0647448a2583a5
dotnetcurry <dotnetcurry@a2zknowledgevisuals.com>
2013-05-03 01:22:17 +0530

    Code Commit

---
+++ WebAPICRUD/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPICRUD
+{
+    public class CustomersController : ApiController
+    {
+        public List<Customer> Get()
+        {
+            return CustomersRepository.GetAllCustomers();
+        }
+
+        public Customer Get(string id)
+        {
+            return CustomersRepository.GetCustomer(id);
+        }
+
+        public void Post([FromBody]Customer customer)
+        {
+            CustomersRepository.InsertCustomer(customer);
+        }
+
+        public void Put([FromBody]Customer customer)
+        {
+            CustomersRepository.UpdateCustomer(customer);
+        }
+
+        public void Delete(string id)
+        {
+            CustomersRepository.DeleteCustomer(id);
+        }
+    }
+}
---
+++ WebAPICRUD/CustomersRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPICRUD
+{
+    public class CustomersRepository
+    {
+        public static List<Customer> GetAllCustomers()
+        {
+            SampleDBEntities dataContext=new SampleDBEntities();
+            var query = from customer in dataContext.Customers
+                        select customer;
+            return query.ToList();
+        }
+        public static Customer GetCustomer(string customerID)
+        {
+            SampleDBEntities dataContext=new SampleDBEntities();
+            var query = (from customer in dataContext.Customers
+                         where customer.CustomerID==customerID
+                         select customer).SingleOrDefault();
+            return query;
+        }
+        public static void InsertCustomer(Customer newCustomer)
+        {
+            SampleDBEntities dataContext = new SampleDBEntities();
+            dataContext.Customers.Add(newCustomer);
+            dataContext.SaveChanges();
+        }
+        public static void UpdateCustomer(Customer oldCustomer)
+        {
+            SampleDBEntities dataContext = new SampleDBEntities();
+            var query = (from customer in dataContext.Customers
+                         where customer.CustomerID==oldCustomer.CustomerID
+                         select customer).SingleOrDefault();
+            query.CustomerID = oldCustomer.CustomerID;
+            query.ContactName = oldCustomer.ContactName;
+            query.City = oldCustomer.City;
+            dataContext.SaveChanges();
+        }
+        public static void DeleteCustomer(string customerID)
+        {
+            SampleDBEntities dataContext = new SampleDBEntities();
+            var query = (from customer in dataContext.Customers
+                         where customer.CustomerID == customerID
+                         select customer).SingleOrDefault();
+            dataContext.Customers.Remove(query);
+            dataContext.SaveChanges();
+        }
+    }
+}
